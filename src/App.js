85dbb6b08763fb404894056298515e209f08fc6e
dibashthapa <dibashthapa55@gmail.com>
2020-06-09 19:00:54 +0545

    First React Todo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos'
+import Form from './components/Form'
+class App extends React.Component{
+  state ={
+    todos:[
+      {id:1, content:"buy some milk"},
+      {id:2, content:"play some mario cart"}
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  deleteTodo =(id) =>{
+    const todos = this.state.todos.filter(
+      todo=>{
+        return todo.id !== id
+      }
+    )
+    this.setState({
+    todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos =[...this.state.todos , todo]
+    this.setState({
+      todos:todos
+    })
+
+  }
+  render(){
+    return(
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <Form  addTodo = {this.addTodo}/>        
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
